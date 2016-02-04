f9c4374b2b3dd5984eb49d8b9dc26a8800071230
Scott Weaver <scottmweaver@gmail.com>
2016-02-03 16:45:53 -0800

    First commit: added dictionary look-up command

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-var Botkit = require('botkit')
+var Botkit = require('botkit'),
+http = require('http'),
+libxmljs = require('libxmljs');
 
 // Expect a SLACK_TOKEN environment variable
 var slackToken = process.env.SLACK_TOKEN
@@ -18,6 +20,50 @@ bot.startRTM(function (err, bot, payload) {
   }
 })
 
+controller.hears('define (.+)', ['direct_message', 'direct_mention'], function (bot, message) {
+  var reference = 'collegiate',
+  references = 
+  word = message.match[1];
+
+  var websterApiKey = process.env.WEBSTER_API_KEY;
+
+  if (!websterApiKey) {
+    console.error('WEBSTER_API_KEY is required!');
+    process.exit(1);
+  }
+
+  http.get('http://www.dictionaryapi.com/api/v1/references/' + encodeURIComponent(reference) + '/xml/' + encodeURIComponent(word) + '?key=' + encodeURIComponent(process.env.WEBSTER_API_KEY), function(res) {
+
+    var body = '';
+
+    res.on('data', function(chunk) {
+      body += chunk;
+    });
+
+    res.on('end', function() {
+
+      try {
+        var xmlDoc = libxmljs.parseXml(body),
+        entries = xmlDoc.root().childNodes(),
+        entry = entries[0],
+        typeObj = entry.get('//fl'),
+        definitionObj = entry.get('//def/dt'),
+        type = typeObj.text(),
+        definition = definitionObj.text().replace(':', '');
+
+        bot.reply(message, '<@' + message.user + '> ' + word + ' ('+ type +'): ' + definition);
+        console.log(word + '<@' + message.user + '> ' + word + ' ('+ type +'): ' + definition);
+      } catch (ex) {
+        bot.reply(message, 'Sorry, <@'+ message.user +'>, I could not look up that word. Are you sure you typed it correctly?');
+        console.error(ex);
+      }
+
+    });
+  }).on('error', function(e) {
+    console.log('Got error: ' + e.message);
+  });
+})
+
 controller.on('bot_channel_join', function (bot, message) {
   bot.reply(message, "I'm here!")
 })
@@ -25,6 +71,7 @@ controller.on('bot_channel_join', function (bot, message) {
 controller.hears(['hello', 'hi'], ['direct_mention'], function (bot, message) {
   bot.reply(message, 'Hello.')
 })
+
 
 controller.hears(['hello', 'hi'], ['direct_message'], function (bot, message) {
   bot.reply(message, 'Hello.')
@@ -37,10 +84,10 @@ controller.hears('.*', ['mention'], function (bot, message) {
 
 controller.hears('help', ['direct_message', 'direct_mention'], function (bot, message) {
   var help = 'I will respond to the following messages: \n' +
-      '`bot hi` for a simple message.\n' +
-      '`bot attachment` to see a Slack attachment message.\n' +
-      '`@<your bot\'s name>` to demonstrate detecting a mention.\n' +
-      '`bot help` to see this again.'
+  '`bot hi` for a simple message.\n' +
+  '`bot attachment` to see a Slack attachment message.\n' +
+  '`@<your bot\'s name>` to demonstrate detecting a mention.\n' +
+  '`bot help` to see this again.'
   bot.reply(message, help)
 })
 
